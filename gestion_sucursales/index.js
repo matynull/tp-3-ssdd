427ffe5ed4dc2aaf9c25c49dc83d038c71207e16
Luca <lucagutierrez@hotmail.com>
2022-10-23 21:51:56 -0300

    Parsing de url incompleto

--- gestion_sucursales/index.js
+++ gestion_sucursales/index.js
@@ -3,7 +3,21 @@ const http = require('http');
 const server = http.createServer(function (req, res) {
     
     console.log(req.url);
-    res.end('Mundo');
+
+    if(req.url.startsWith("/api/sucursales")){
+        let urlArr = req.url.split("/");
+        console.log(urlArr);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        if(urlArr.length >= 3){
+            res.end(JSON.stringify({"id" : urlArr[2], "contenido" : "Una sola sucursal"}));
+        }else{
+            res.end(JSON.stringify({"contenido" : "Todas las sucursales"}));
+        }
+    }else{
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end("No se encontro el recurso.");
+    }
+    
 });
 
 server.listen(8080, function() {
