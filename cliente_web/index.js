958667f42bd565ebcda7ee4cefaeac06f7c5aa13
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2022-11-07 15:21:40 -0300

    formulario

--- cliente_web/index.js
+++ cliente_web/index.js
@@ -1,36 +1,9 @@
-const fetch =require("node-fetch")
+function recibeDatos(){
+    var email=document.getElementById("email").value;
+    var fecha=document.getElementById("fecha").value;
+    var horario=document.getElementById("horario").value;
+    var sucursal=document.getElementById("sucursales").value;
 
+    console.log(email,fecha,horario,sucursal);
 
-
-
-// async function getData(url = '', data = {}) {
-//     const response = await fetch(url, {
-//       method: 'GET', 
-//       mode: 'no-cors', 
-//       cache: 'no-cache', 
-//       credentials: 'same-origin', 
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//       },
-//       redirect: 'follow', 
-//       referrerPolicy: 'no-referrer',
-//       body: JSON.stringify(data) 
-//     });
-//     return response.json(); 
-//   }
-//   //hago la prueba con el demo map del github
-//   getData('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec', { answer: 42 })
-//     .then((data) => {
-//       console.log(data); 
-//     });
-
-
-
-
-
-fetch('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Output: ', data);
-}).catch(err => console.error(err));
+}
