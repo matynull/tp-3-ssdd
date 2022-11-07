0d310dbdfa14f76ff4d7be88390d571ee68854e5
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2022-11-07 13:54:40 -0300

    avances api cartes

--- cliente_web/index.js
+++ cliente_web/index.js
@@ -1,8 +1,36 @@
+const fetch =require("node-fetch")
 
 
-fetch('https://app.cartes.io/maps/695eec4d-cec4-40e5-9d20-c6fca874a941?lat=-37.96305032495199&lng=-57.574300467967994&zoom=18')
-    .then(result => result.json())
-    .then((output) => {
-        console.log('Output: ', output);
-        
+
+
+// async function getData(url = '', data = {}) {
+//     const response = await fetch(url, {
+//       method: 'GET', 
+//       mode: 'no-cors', 
+//       cache: 'no-cache', 
+//       credentials: 'same-origin', 
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       redirect: 'follow', 
+//       referrerPolicy: 'no-referrer',
+//       body: JSON.stringify(data) 
+//     });
+//     return response.json(); 
+//   }
+//   //hago la prueba con el demo map del github
+//   getData('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec', { answer: 42 })
+//     .then((data) => {
+//       console.log(data); 
+//     });
+
+
+
+
+
+fetch('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Output: ', data);
 }).catch(err => console.error(err));
