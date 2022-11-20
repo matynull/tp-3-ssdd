524901a06e3eed7a3f41fd65e7e470201228de4b
mati <matiasalmeida546@gmail.com>
2022-11-20 03:25:45 -0300

    Se agrego getReservas

--- gestion-reservas/index.js
+++ gestion-reservas/index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const solicitar = require('./solicitar.js');
 const resetReserva = require('./resetReserva.js');
@@ -15,7 +17,7 @@ const server = http.createServer(async (req, res) => {
     let urlArr = parsedUrl.split("/");
 
     if (req.url.startsWith("/api/reservas/confirmar") && req.method == 'POST') {
-        confirmar(req,res);
+        confirmar(req, res);
         let idReserva = urlArr[3];
         let reservaConfirmada = queueReservas.find(x => x.idReserva == idReserva);
         let index = queueReservas.findIndex((i) => i.idReserva == reservaConfirmada.idReserva);
@@ -35,7 +37,30 @@ const server = http.createServer(async (req, res) => {
             queueReservas.push({ idReserva: idReserva, idTimeOut: idTimeOut });
         })
     } else if (req.url.startsWith("/api/reservas") && req.method == 'GET') {
-        getReservas(req, res);
+        if (urlArr.length == 3 && !isNaN(urlArr[2])) {
+            const data = fs.readFileSync('./gestion-reservas/turnos.json',
+                { encoding: 'utf8', flag: 'r' });
+            let reservas = JSON.parse(data);
+            let idReserva = parseInt(urlArr[2]);
+            let reserva = reservas.find(x => x.idReserva == idReserva);
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(reserva));
+        } else {
+            const data = fs.readFileSync('./gestion-reservas/turnos.json',
+                { encoding: 'utf8', flag: 'r' });
+            let reservas = JSON.parse(data);
+            //const objParam = { userId: null, branchId: null, dateTime: null }
+            queryParams = url.parse(req.url, true).query;
+            let results = reservas.filter(function (entry) {
+                return Object.keys(queryParams).every(function (key) {
+                    return entry[key] === queryParams[key];
+                });
+            });
+            console.log(results);
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(results));
+        }
+
     }
     else {
         res.writeHead(404, { "Content-Type": "application/json" });
@@ -44,8 +69,13 @@ const server = http.createServer(async (req, res) => {
 
 });
 
-
-
+const findByMatchingProperties = (set, properties) => {
+    return set.filter(function (entry) {
+        return Object.keys(properties).every(function (key) {
+            return entry[key] === properties[key];
+        });
+    });
+}
 server.listen(8089, function () {
     console.log('Server started');
 });
