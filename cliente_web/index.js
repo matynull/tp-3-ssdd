24e02db38de785b29611c8eb22b0be1e9950e54f
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2022-11-20 19:00:29 -0300

    interfaz

--- cliente_web/index.js
+++ cliente_web/index.js
@@ -3,7 +3,41 @@ function recibeDatos(){
     var fecha=document.getElementById("fecha").value;
     var horario=document.getElementById("horario").value;
     var sucursal=document.getElementById("sucursales").value;
+    var botonenv =document.getElementById("btnenv").value;
 
-    console.log(email,fecha,horario,sucursal);
+   
+
+    console.log(email,"\n",fecha,"\n",horario,"\n",sucursal);
+
+    botonenv.addEventListener("click",function(event){
+        const res=fetch
+    });
 
 }
+
+//POST /api/
+const req= fetch(`https://localhost:1010/api`, {
+    method: "POST",
+    headers:" 'Accept': 'application/json'",
+    body:JSON.stringify({
+        "dateTime":date,//aca va fecha con hora
+        "userId":0, //0
+        "email":email,
+        "branchId":sucursal
+    })
+
+}).then(res=>{
+    if(res.status==200){
+
+    }
+    else{
+        console.log("error")
+    }
+})
+
+
+//DELETE /api/
+
+//GET /api/
+
+// GET /api/sucursales
